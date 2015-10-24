930b7ad7488a8f0f7acd2cb039f512b2c087287e
Mac Chow <zhouxinghai@youzan.com>
2015-10-24 16:23:48 +0800

    登陆完成

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\User;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,5 +11,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function fetchUserData()
+    {
+        $token = request()->cookie('token');
+        $user = User::where('token', $token)->first();
+        return $user;
+    }
 
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,38 +10,41 @@ class UserController extends Controller
 {
     public function postLogin(Request $req)
     {
-        $phone = $req->get('phone');
-        $name = $req->get('name');
-
-        if (empty($phone)) {
-            return response()->json([
-                'code' => 403,
-                'msg' => '手机号不能为空',
-            ]);
-        }
-
-        $user = User::where('telephone', $phone)->first();
-
+        $user = $this->fetchUserData();
         if (!$user) {
-            if (empty($name)) {
+            $phone = $req->get('phone');
+            $name = $req->get('name');
+
+            if (empty($phone)) {
                 return response()->json([
                     'code' => 403,
-                    'msg' => '用户名不能为空',
+                    'msg' => '手机号不能为空',
                 ]);
             }
-            $user = User::create([
-                'name' => $name,
-                'telephone' => $phone,
-                'token' => uniqid('', true)
-            ]);
+
+            $user = User::where('telephone', $phone)->first();
+
+            if (!$user) {
+                if (empty($name)) {
+                    return response()->json([
+                        'code' => 403,
+                        'msg' => '用户名不能为空',
+                    ]);
+                }
+                $user = User::create([
+                    'name' => $name,
+                    'telephone' => $phone,
+                    'token' => uniqid('', true)
+                ]);
+            }
         }
         return response()
-        ->json([
-            'code' => 0,
-            'msg' => 'success',
-            'data' => []
-        ])
-        ->withCookie('token', $user->token);
+                ->json([
+                    'code' => 0,
+                    'msg' => 'success',
+                    'data' => []
+                ])
+                ->withCookie('token', $user->token);
     }
 
 }
