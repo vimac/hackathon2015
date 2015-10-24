d02a3dca499ac625a54fcfc3e7ee70f070d68b97
xiaolong <xiaolong@qima-inc.com>
2015-10-24 20:35:56 +0800

    签到

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -76,6 +76,51 @@ class ActivityController extends Controller
 
     public function getActivityUser(Request $req) {
 
+    }
+
+    /**
+     * 签到
+     */
+    public function postActivitysign(Request $req) {
+        $user = $this->fetchUserData();
+        $activityId = $req->get('activity_id');
+        $phone = $req->get('phone');
+
+        if ($user) {
+            $userId = $user->id;
+        } else {
+            if ($phone) {
+                $user = User::where('telephone', $phone)->first();
+                if(!$user) {
+                    $user = User::create([
+                        'name' => $phone,
+                        'telephone' => $phone,
+                        'token' => uniqid('', true)
+                    ]);
+                    $userActivity = UserActivity::create([
+                        'user_id' => $user->id,
+                        'activity_id' => $activityId,
+                        'status' => 1
+                    ]);
+                }
+                $userId = $user->id;
+            } else {
+                return response()->json([
+                    'code' => 10100,
+                    'msg' => '签到异常',
+                    'data' => []
+                ]);
+            }
+        }
+        $userActivity = UserActivity::where(['user_id' => $userId, 'activity_id' => $activityId])->first();
+        $userActivity->status = 1;
+        $userActivity = $userActivity->save();
+
+        return response()->json([
+            'code' => 0,
+            'msg' => 'ok',
+            'data' => $userActivity
+        ]);
 
     }
 }
